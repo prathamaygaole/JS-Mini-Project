f19a206d66f332114d486b1a787f23ec47551fb3
prathamaygaole <84084305+prathamaygaole@users.noreply.github.com>
2021-09-22 00:05:53 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,22 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Pratham" && password == "Lucifer@04"){
+alert ("Login successfully");
+window.location.assign("https://www.instagram.com/_l_u_c_i_f_e_r_4_/"); // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
